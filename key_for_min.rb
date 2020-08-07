516c1405cd8ecf3f551ff6349d9a45c9c6583f71
Rebecca Congi <rncongi@gmail.com>
2020-08-07 00:49:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,11 @@ test = ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   correct_key = ""
   i = nil  
-  nums_list = []
+  min_key = nil
   name_hash.each do |key, num|
     if i == nil || num < i
       i = num 
-    
+      min_key = key
     end 
   end
 end
